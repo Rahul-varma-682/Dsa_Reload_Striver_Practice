0de7cd359f6d629331cf5788eda47df263e8d4f4
Rahulvarma <133089009+Rahul-varma-682@users.noreply.github.com>
2026-04-06 19:40:10 +0530

    Implement pattern printing in Pattern1.java
    Add Java program to print asterisks in a NXN matrix pattern using nested loops.

---
+++ patterns/Pattern1.java
@@ -0,0 +1,22 @@
+hai this pattren will be like 
+****
+****
+****
+****
+  for printing this pattern we use two loops one inside other called as nested loops 
+
+
+  java code :
+
+  import java.util.*; 
+ public class MyClass{
+   public static void main(String [] args){
+     Scanner sc = new Scanner(System.in);
+     for (int i =0; i<n;i++){
+       for (int j=0;j<n;j++){
+         System.out.print("*");
+       }
+         System.out.println();
+     }
+   }
+ }
