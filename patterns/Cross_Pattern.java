2b0148d12e41c3ffbcf3b7d6307c6dab956e4f4f
Rahulvarma <133089009+Rahul-varma-682@users.noreply.github.com>
2026-04-07 15:53:02 +0530

    Implement cross pattern printing in Java
    Added a Java program to print a cross pattern based on user input.

---
+++ patterns/Cross_Pattern.java
@@ -0,0 +1,50 @@
+pattern:
+5
+x   x
+ x x 
+  x  
+ x x 
+x   x
+
+
+java code:
+import java.util.Scanner;
+
+public class P_Diagonal_2 {
+     public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+
+                if (i+j==n-1||i==j){
+                    System.out.print("x");
+                }
+                else {
+                    System.out.print(" ");
+                }
+
+
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+    
+    
+}
+
+        }
+
+
+
+
+
+    }
+    
+    
+}
