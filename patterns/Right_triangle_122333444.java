0bb3f85ef9ed668a4daf2888c44601e228d24ce4
Rahulvarma <133089009+Rahul-varma-682@users.noreply.github.com>
2026-04-10 12:16:15 +0530

    Add Right_triangle_122333444.java for pattern printing
    Implement a Java program to print a right triangle pattern based on user input.

---
+++ patterns/Right_triangle_122333444.java
@@ -0,0 +1,27 @@
+pattern :
+5
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+java code : 
+import java.util.*;
+public class Right_triangle_122333444 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+
+        
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
+
